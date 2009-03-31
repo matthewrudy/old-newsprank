a45b9ed262c5be6a09adf341acd1b364b3683074
Matthew Rudy Jacobs <matthewrudyjacobs@gmail.com>
2009-03-31 01:00:11 +0100

    make the /notd paths work like a resource   /notd/23 shows the story will be used with a permalink soon

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :stories
+  map.resources :notd, :controller => "notd"
 
   # The priority is based upon order of creation: first created -> highest priority.
 
